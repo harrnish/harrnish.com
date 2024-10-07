284c7f4bd3916a5425e4e0c3de74dfff098676f3
Harnish <harrnish@gmail.com>
2024-10-07 17:38:30 -0400

    Bio

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import "./App.css";
+import ProfilePicture from "./assets/photos/profile-picture.jpg";
 
 function App() {
   return (
     <>
-      <div className="app"></div>
+      <div className="app">
+        <div className="container">
+          <div className="bio">
+            <div className="profile-picture">
+              <img src={ProfilePicture} alt="Profile picture" />
+            </div>
+            <div className="bio-copy">
+              <h1>Harnish Mistry</h1>
+              <p>Visual Developer based in Toronto, ON</p>
+              <div className="callout">
+                <a href="https://www.linkedin.com/in/harrnish/" target="_blank">
+                  linkedin.com/in/harrnish
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
